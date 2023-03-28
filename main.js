80e358a8b342fc6b12f34aa8ec71bda14a2db414
Mpiloenhle thandolwethu Ntanda <125492323+Codedeveloper-MT@users.noreply.github.com>
2023-03-28 16:33:40 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+function ClickedBtn() {
+  document.getElementById('SubTopic').innerHTML='the Button is been Clicked.';
+  document.getElementById('SubTopic').style.textDecoration='underline';
+  document.getElementById('SubTopic').style.color='lime';
+  document.getElementById('BtnClicked').innerHTML="hellow you welcome";
+  document.getElementById('btn').style.display='none';
+ }
